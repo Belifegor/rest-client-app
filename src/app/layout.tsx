0c3745bd68afa67dd4707de0e289390c7aa90f95
ann-sm <annsmirnovva@gmail.com>
2025-09-10 20:54:04 +0300

    chore: set up next-intl

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "@/app/globals.css";
 import type { Metadata } from "next";
 import AppShell from "@/components/layout/AppShell";
+import { NextIntlClientProvider } from "next-intl";
 
 export const metadata: Metadata = { title: "REST Client", description: "Mini Postman on Next.js" };
 
@@ -8,7 +9,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body className="min-h-dvh bg-gray-900 text-white antialiased">
-        <AppShell>{children}</AppShell>
+        <NextIntlClientProvider>
+          <AppShell>{children}</AppShell>
+        </NextIntlClientProvider>
       </body>
     </html>
   );
